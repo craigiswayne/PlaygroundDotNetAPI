bcd0e7c575bc10bbc2cea4c5d4ca6589a4578e28
Craig Wayne <craigiswayne@gmail.com>
2024-11-27 21:39:01 +0000

    resetting files

--- PlaygroundDotNetAPI/Controllers/EnvironmentController.cs
+++ PlaygroundDotNetAPI/Controllers/EnvironmentController.cs
@@ -17,10 +17,6 @@ public class EnvironmentController(IConfiguration configuration, IWebHostEnviron
     {
         var env = new
         {
-            // seems to be case-insensitive
-            mySecretLowerCase = configuration.GetValue<string>("my_secret"),
-            mySecret = configuration.GetValue<string>("MY_SECRET"),
-
             hostEnvironmentName = hostEnvironment.EnvironmentName,
             allowedOrigins = configuration.GetSection("AllowedOrigins").Get<List<string>>(),
             allowedHosts = configuration.GetValue<string>("AllowedHosts"),
