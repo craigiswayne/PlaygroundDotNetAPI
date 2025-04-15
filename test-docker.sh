6d3080e2892c5083fc98de45ca4db8cc0664b72b
Craig Wayne <craigiswayne@gmail.com>
2025-04-15 03:07:52 +0100

    Run docker with different environments

--- test-docker.sh
+++ test-docker.sh
@@ -1,6 +1,8 @@
 docker build -t playground-dotnet-api -f PlaygroundDotNetAPI/Dockerfile .
 docker rm playground-dotnet-api
-# This will build prod
-# docker run --name=playground-dotnet-api -p 7137:8080 playground-dotnet-api
-
-docker run --name=playground-dotnet-api -p 7137:8080 playground-dotnet-api --build-arg BUILD_CONFIGURATION="Debug"
+# Development --> appsettings.Development.json
+docker run --name=playground-dotnet-api -p 7137:8080 --env DOTNET_ENVIRONMENT="Development" playground-dotnet-api
+# Staging --> appsettings.Staging.json
+#docker run --name=playground-dotnet-api -p 7137:8080 --env DOTNET_ENVIRONMENT="Staging" playground-dotnet-api
+# Production --> appsettings.json
+#docker run --name=playground-dotnet-api -p 7137:8080 playground-dotnet-api
